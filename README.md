a8cf7b6e6d01fb25e46401c1b04739e16c3ef2e0
Nicholas Hoo <nickhoo10@gmail.com>
2024-01-06 18:45:35 -0500

    routes finished

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # MERN-todo
  Title: MERN todo list app
+ - Mongo
+ - Express
+ - React
+ - Node.js
  Author: Nick Hoo
--- api/models/Todo.js
+++ api/models/Todo.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const TodoSchema = new SchemaType({
+
+const TodoSchema = new Schema({
     text: {
         type: String,
         required: true
