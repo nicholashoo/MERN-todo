e98b840f7d53194e8765775f04a83f41d95461d7
Nicholas Hoo <nickhoo10@gmail.com>
2024-01-07 17:33:21 -0500

    css done and hardcoded todo list to check styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todos, setTodos] = useState([]);
+    const [popupActive, setPopupActive] = useState(false);
+    const [newTodo, setNewTodo] = useState("");
+
+    return (
+        <div className="App">
+            <h1>Welcome, Nick</h1>
+            <h4>Your Tasks</h4>
+
+            <div className="todos">
+                <div className="todo">
+                    <div className="checkbox"></div>
+                    <div className="text">Play with Eddie</div>
+                    <div className="delete-todo">x</div>
+                </div>
+                <div className="todo is-complete">
+                    <div className="checkbox"></div>
+                    <div className="text">Call Dad</div>
+                    <div className="delete-todo">x</div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
