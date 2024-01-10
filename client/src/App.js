5d1b62242048967a7a54603e7eb5b9fa4e30bc9f
Nicholas Hoo <nickhoo10@gmail.com>
2024-01-10 18:07:40 -0500

    todos displayed based on database + clicking to complete works

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,37 @@
 import { useEffect, useState } from "react";
 
+const API_BASE = "http://localhost:3001"
+
 function App() {
     const [todos, setTodos] = useState([]);
     const [popupActive, setPopupActive] = useState(false);
     const [newTodo, setNewTodo] = useState("");
+
+    const completeTodo = async id => {
+        const data = await fetch(API_BASE + "/todo/complete/" + id)
+            .then(res => res.json());
+
+        setTodos(todos => todos.map(todo => {
+            if (todo._id === data._id) {
+                todo.complete = data.complete;
+            }
+
+            return todo;
+        }))
+    }
+
+    useEffect(() => {
+        GetTodos();
+
+        console.log(todos);
+    }, [])
+
+    const GetTodos = async () => {
+        fetch(API_BASE + "/todos")
+            .then(res => res.json())
+            .then(data => setTodos(data))
+            .catch(err => console.error("Error: ", err));
+    }
 
     return (
         <div className="App">
@@ -11,16 +39,15 @@ function App() {
             <h4>Your Tasks</h4>
 
             <div className="todos">
-                <div className="todo">
-                    <div className="checkbox"></div>
-                    <div className="text">Play with Eddie</div>
-                    <div className="delete-todo">x</div>
-                </div>
-                <div className="todo is-complete">
-                    <div className="checkbox"></div>
-                    <div className="text">Call Dad</div>
-                    <div className="delete-todo">x</div>
-                </div>
+                {todos.map(todo => (
+                    <div className={
+                        "todo " + (todo.complete ? "is-complete" : "")
+                        } key={todo._id} onClick={() => completeTodo(todo._id)}>
+                            <div className="checkbox"></div>
+                            <div className="text"> { todo.text } </div>
+                            <div className="delete-todo">x</div>
+                    </div>
+                ))}
             </div>
         </div>
     );
